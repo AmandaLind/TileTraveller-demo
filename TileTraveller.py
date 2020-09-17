7a10b0882a6f286a2bde73f52ec7c15c8c8527ff
Bjarmi99 <bjarmib19@ru.is>
2020-09-17 16:22:38 +0000

    Bætti við algorithmann

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,4 +1,6 @@
 #Algorithmi:
 #Skilgreinum eina tölu, hún hefur gildið 1.1 í upphafi
-#Búum til níu mismunandi föll, hvert og eitt þeirra segir til um hvað á sér stað þegar leikmaður er í ákveðnum reit.
+#Búum til fall fyrir hvern og einn reit nema þann síðasta (3,1), hvert og eitt fall segir til um hvað á sér stað þegar leikmaður er í ákveðnum reit.
 #Þegar við köllum á föllin og leikmaður velur "réttar áttir" þá breytist gildið á tölunni, þar sem leikmaður hefur nú færst um reit.
+#Notum if-setningar til þess að kalla á viðeigandi föll
+#Setjum while lykkju í kringum þær if-setningar svo að hægt sé að hreyfa sig að vild, þar til leikmaður kemst í reit 3,1
