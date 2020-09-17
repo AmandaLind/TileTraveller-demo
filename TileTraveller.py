09334a3a676b2754480163f764cb50284b70b4b8
AmandaLind <amanda19@ru.is>
2020-09-17 14:41:47 +0000

    Algorithmi tilbúinn

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,1 +1,4 @@
-print("Hello)
+#Algorithmi:
+#Skilgreinum eina tölu, hún hefur gildið 1.1 í upphafi
+#Búum til níu mismunandi föll, hvert og eitt þeirra segir til um hvað á sér stað þegar leikmaður er í ákveðnum reit.
+#Þegar við köllum á föllin og leikmaður velur "réttar áttir" þá breytist gildið á tölunni, þar sem leikmaður hefur nú færst um reit.
